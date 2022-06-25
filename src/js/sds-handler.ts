986c4210874c7e7d600cd305279c84402747f93f
Nathan <novadrake76@gmail.com>
2022-06-25 17:41:33 -0300

    fix: sds-form page & details refactored product card created

--- src/js/sds-handler.ts
+++ src/js/sds-handler.ts
@@ -1,11 +1,11 @@
-const button = document.getElementById('btn-documents')
-const step1 = document.getElementById('sds-step-1')
-const step2 = document.getElementById('sds-step-2')
+// const button = document.getElementById('btn-documents')
+// const step1 = document.getElementById('sds-step-1')
+// const step2 = document.getElementById('sds-step-2')
 
-button?.addEventListener('click', function handleClick(event) {
-  step1?.classList.remove('flex')
-  step1?.classList.add('hidden')
+// button?.addEventListener('click', function handleClick(event) {
+//   step1?.classList.remove('flex')
+//   step1?.classList.add('hidden')
 
-  step2?.classList.remove('hidden')
-  step2?.classList.add('flex')
-})
+//   step2?.classList.remove('hidden')
+//   step2?.classList.add('flex')
+// })
